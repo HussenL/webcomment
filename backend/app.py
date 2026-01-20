488c47aeb81740941900f39f8e3a2f91ea6eddf2
HussenL <hussen_apply@163.com>
2026-01-20 22:34:00 +0800

    add docker

--- backend/app.py
+++ backend/app.py
@@ -326,3 +326,4 @@ async def events(request: Request):
 @app.exception_handler(HTTPException)
 async def http_exc_handler(_, exc: HTTPException):
     return JSONResponse(status_code=exc.status_code, content={"ok": False, "detail": exc.detail})
+
